d7105e980dd23748f51706820520a589923f9833
ElRodFe <el.rod.fe@gmail.com>
2024-04-26 13:15:32 -0300

    Revert "addToCart function fixed"
    This reverts commit b470c3c752a2d039fc92fe9ee9589789dda39cf4.

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,11 @@
 import { setLocalStorage } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let productsList = getLocalStorage("so-cart");
-
-  if (!Array.isArray(productsList)) {
-    productsList = [];
-  }
-
-  productsList.push(product);
-
-  setLocalStorage("so-cart", productsList);
+  setLocalStorage("so-cart", product);
 }
-
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
