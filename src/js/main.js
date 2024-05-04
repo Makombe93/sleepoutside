17bb1d7165b01d900783bef748c9278d66a5b465
amandamigliorini <amanda.migliorini@hotmail.com>
2024-05-04 12:00:33 -0300

    correcting main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,7 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 
 const productData = new ProductData("tents");
-const productList = new ProductListing();
+const element = document.querySelector(".product-list");
+const productList = new ProductListing("Tents", productData, element);
+
+productList.init();
