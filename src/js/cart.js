1ab53d064e4eddbcff8cb408b9d25d6071ad7086
amandamigliorini <amanda.migliorini@hotmail.com>
2024-05-09 22:18:39 -0300

    Team Activity Week 3

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,35 +3,35 @@
 //loadHeaderFooter();
 
 //function renderCartContents() {
-  //const cartItems = getLocalStorage("so-cart");
+//const cartItems = getLocalStorage("so-cart");
 
-  //if (cartItems == null) {
-    //const p = document.createElement("p");
-    //p.textContent = "No items yet";
-    //document.querySelector(".product-list").appendChild(p);
-  //} else {
-    //const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    //document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  //}
+//if (cartItems == null) {
+//const p = document.createElement("p");
+//p.textContent = "No items yet";
+//document.querySelector(".product-list").appendChild(p);
+//} else {
+//const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+//document.querySelector(".product-list").innerHTML = htmlItems.join("");
+//}
 //}
 
 //function cartItemTemplate(item) {
-  //const newItem = `<li class="cart-card divider">
-  //<a href="#" class="cart-card__image">
-    //<img
-      //src="${item.Image}"
-      //alt="${item.Name}"
-    ///>
-  //</a>
-  //<a href="#">
+//const newItem = `<li class="cart-card divider">
+//<a href="#" class="cart-card__image">
+//<img
+//src="${item.Image}"
+//alt="${item.Name}"
+///>
+//</a>
+//<a href="#">
 //    <h2 class="card__name">${item.Name}</h2>
-  //</a>
-  //<p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  //<p class="cart-card__quantity">qty: 1</p>
-  //<p class="cart-card__price">$${item.FinalPrice}</p>
+//</a>
+//<p class="cart-card__color">${item.Colors[0].ColorName}</p>
+//<p class="cart-card__quantity">qty: 1</p>
+//<p class="cart-card__price">$${item.FinalPrice}</p>
 //</li>`;
 
-  //return newItem;
+//return newItem;
 //}
 
 //renderCartContents();
