b717bdd02a2de0923f9f572681515002269c641a
amandamigliorini <amanda.migliorini@hotmail.com>
2024-05-10 18:07:27 -0300

    First commit indidual3

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -27,7 +27,6 @@ export function getParams(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get(param);
-
   return product;
 }
 
